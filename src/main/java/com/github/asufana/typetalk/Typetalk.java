cca75a3f771293accebcaaca502ff471ca0c4c2e
hana <asufanah@gmail.com>
2015-03-22 00:01:48 +0900

    Added invalid connect params test.

--- src/main/java/com/github/asufana/typetalk/Typetalk.java
+++ src/main/java/com/github/asufana/typetalk/Typetalk.java
@@ -8,9 +8,11 @@ import com.google.gson.reflect.*;
 import com.squareup.okhttp.*;
 
 public class Typetalk {
-    
     private final OkHttpClient client = new OkHttpClient();
+    
     private static final String BASE_URL = "https://typetalk.in";
+    private static final String CONNECT_URL = BASE_URL + "/oauth2/access_token";
+    
     private final String clientId;
     private final String clientSecret;
     private final String accessToken;
@@ -22,29 +24,30 @@ public class Typetalk {
     }
     
     private String connect() {
-        final RequestBody formBody = new FormEncodingBuilder().add("client_id",
-                                                                   clientId)
-                                                              .add("client_secret",
-                                                                   clientSecret)
-                                                              .add("grant_type",
-                                                                   "client_credentials")
-                                                              .add("scope",
-                                                                   "my")
-                                                              .build();
-        final Request request = new Request.Builder().url(BASE_URL
-                + "/oauth2/access_token")
-                                                     .post(formBody)
+        final Request request = new Request.Builder().url(CONNECT_URL)
+                                                     .post(connectParams())
                                                      .build();
         try {
             final Response response = client.newCall(request).execute();
-            final String jsonString = response.body().string();
-            final Map<String, String> json = new Gson().fromJson(jsonString,
+            if (response.code() != 200) {
+                throw new RuntimeException(response.body().string());
+            }
+            final Map<String, String> json = new Gson().fromJson(response.body()
+                                                                         .string(),
                                                                  new TypeToken<Map<String, String>>() {}.getType());
             return json.get("access_token");
         }
         catch (final IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    protected RequestBody connectParams() {
+        return new FormEncodingBuilder().add("client_id", clientId)
+                                        .add("client_secret", clientSecret)
+                                        .add("grant_type", "client_credentials")
+                                        .add("scope", "my")
+                                        .build();
     }
     
     public void profile() {
--- src/test/java/com/github/asufana/typetalk/TypetalkTest.java
+++ src/test/java/com/github/asufana/typetalk/TypetalkTest.java
@@ -2,15 +2,34 @@ package com.github.asufana.typetalk;
 
 import org.junit.*;
 
+import com.squareup.okhttp.*;
+
 public class TypetalkTest {
     
     public static final String CLIENT_ID = "RW7Xi2PKyGMtzoOPIu3qxYe02gMcPfGQ";
     public static final String CLIENT_SECRET = "RDDGLi3kEBHv041MiotTbEQovHig8AL03eEgprhG8xQLPW6U9MxiLcahm2lPDlCB";
     
+    @Test(expected = RuntimeException.class)
+    public void testConnectionError() throws Exception {
+        new InvalidConnectParamsTypetalk(CLIENT_ID, CLIENT_SECRET);
+    }
+    
     @Test
     public void test() {
         final Typetalk typetalk = new Typetalk(CLIENT_ID, CLIENT_SECRET);
         typetalk.profile();
+    }
+    
+    static class InvalidConnectParamsTypetalk extends Typetalk {
+        public InvalidConnectParamsTypetalk(final String clientId,
+                final String clientSecret) {
+            super(clientId, clientSecret);
+        }
+        
+        @Override
+        protected RequestBody connectParams() {
+            return null;
+        }
     }
     
 }
